5761464cdf1cea2a25c5c47226d354dc4a23160a
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-06 14:56:24 -0500

    adding character set variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = [> 7; < 129];
+var lowLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var capLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var specialChar = ['~','!','@','#','$','%','^','&','*','-','+','=','\','/','.',',',':',';','?']
 
 function generatePassword() {
   // Need the following variables:
